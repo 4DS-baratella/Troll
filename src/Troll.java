e7d932c5cc99611fb0fc599178ee8b2b48a6cb9c
Alex <alex.baratella@buonarroti.tn.it>
2023-10-04 16:21:59 +0200

    feat: creazione metodo costruttore Troll con gestione eccezione

--- src/Troll.java
+++ src/Troll.java
@@ -1,3 +1,11 @@
 public class Troll {
     private int vita;
+
+    public Troll(int vita) throws Exception{
+        if(vita > 0){
+            this.vita = vita;
+        }else{
+            throw new Exception("\nLa vita non può essere minore di 0.");
+        }
+    }
 }
