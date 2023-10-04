63c300b90f65876d97b30be2d2e541573b59c0bf
Alex <alex.baratella@buonarroti.tn.it>
2023-10-04 16:22:58 +0200

    feat: istanziazione della classe Troll nel main e gestione eccezione

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        try{
+            Troll troll = new Troll(100);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
