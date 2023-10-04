62242baf723b34277d332ba892b333be96563b0f
Alex <alex.baratella@buonarroti.tn.it>
2023-10-04 16:41:37 +0200

    fix: condizione do-while per attaccare il troll per controllare la scelta

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
                 }catch (InputMismatchException e){
                     System.out.println("\nLa scelta deve essere un numero intero.");
                 }
-            }while (!checkScelta);
+            }while (!checkScelta || scelta < 1 || scelta > 2);
 
             switch (scelta) {
                 case 1:
