3ad901af5f620b7b986f3ea74e909db83bb8dd5b
Alex <alex.baratella@buonarroti.tn.it>
2023-10-04 16:31:25 +0200

    feat: loop per la scelta dell'attacco

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int scelta = 0;
+        boolean checkScelta = false;
+
         Troll troll = null;
         try{
             troll = new Troll(100);
@@ -8,6 +15,17 @@ public class Main {
         }
 
         while(troll.ancoraVivo()){
+            do{
+                System.out.println("\nInserire l'attacco che si vuole eseguire: " +
+                        "\n1. Attacco normale;" +
+                        "\n2. Attacco pesante;");
+                try{
+                    input = new Scanner(System.in);
+                    scelta = input.nextInt();
+                }catch (InputMismatchException e){
+                    System.out.println("\nLa scelta deve essere un numero intero.");
+                }
+            }while (checkScelta);
 
         }
     }
