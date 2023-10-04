a9b2acf5e1a32e9b8600d46b56903df160ec9640
Alex <alex.baratella@buonarroti.tn.it>
2023-10-04 16:33:10 +0200

    feat: switch per la gestione degli attacchi

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int scelta = 0;
-        boolean checkScelta = false;
+        boolean checkScelta;
 
         Troll troll = null;
         try{
@@ -16,17 +16,23 @@ public class Main {
 
         while(troll.ancoraVivo()){
             do{
+                checkScelta = false;
                 System.out.println("\nInserire l'attacco che si vuole eseguire: " +
                         "\n1. Attacco normale;" +
                         "\n2. Attacco pesante;");
                 try{
                     input = new Scanner(System.in);
                     scelta = input.nextInt();
+                    checkScelta = true;
                 }catch (InputMismatchException e){
                     System.out.println("\nLa scelta deve essere un numero intero.");
                 }
             }while (checkScelta);
 
+            switch (scelta) {
+                case 1 -> troll.attaccoSubito(10);
+                case 2 -> troll.attaccoSubito(25);
+            }
         }
     }
 }
